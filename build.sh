af0c9ec63e1ea12221cc5d8cdaaf73a0b9579d4a
cmdocmd <55096480+cmdocmd@users.noreply.github.com>
2021-05-09 12:04:08 +0400

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-cd "enet"
-cmake CMakeLists.txt
-make
-cp libenet.a ../libenet.a
-cd ..
-g++ -Wall -I/usr/include/cppconn -o "server" "main.cpp" -std=c++11 -L. -lboost_serialization -lboost_system -lenet -iquote. -L/usr/lib -lmysqlcppconn
